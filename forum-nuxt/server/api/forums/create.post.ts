c5a52144f45b857736d1f6cedffd6ddaab7a5dc3
dekersab3u <thomas.de-kersabiec1@etu.univ-lorraine.fr>
2025-06-17 19:23:12 +0200

    mauvais fichier commit rpecedent

--- forum-nuxt/server/api/forums/create.post.ts
+++ forum-nuxt/server/api/forums/create.post.ts
@@ -1,14 +1,10 @@
 import {db} from '~/server/db/client'
-import {forums, insertForumSchema} from '~/server/db/schema'
+import {forums} from '~/server/db/schema'
 import {broadcast} from '~/server/ws'
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
 
-    const forumData = insertForumSchema.parse({
-        nom: body.nom,
-        description: body.description ?? '',
-    })
 
     const forumId = result.insertId
 
