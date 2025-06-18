c2e4c4b62e88726812908727a08b0a4024bbce42
dekersab3u <thomas.de-kersabiec1@etu.univ-lorraine.fr>
2025-06-18 20:25:41 +0200

    pagination updated

--- forum-nuxt/pages/forums/[id].vue
+++ forum-nuxt/pages/forums/[id].vue
@@ -4,12 +4,28 @@
     <p>{{ forum?.description }}</p>
     <hr />
 
-    <!-- Liste des sujets (à venir) -->
+    <v-pagination
+        v-model="pageLocal"
+        :length="totalPages"
+        @update:model-value="changePage"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
 const forumId = route.params.id
+const page = computed(() => Number(route.query.page || 1))
+
+const { data: sujets } = await useFetch(`/api/forums/${forumId}?page=${page.value}`)
 const { data: forum } = await useFetch(`/api/forums/${forumId}`)
+const setup = ( => {
+  const pageLocal = ref(page.value)
+  const totalPages = 10 // à adapter
+
+  const changePage = (newPage: number) => {
+    navigateTo({ query: { page: newPage } })
+  }
+})
+
 </script>
